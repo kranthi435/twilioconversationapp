117703b6ee727f3b74efc45c0312553c896b14d2
Kranthi <kpeddi@aligntech.com>
2021-04-15 19:35:02 +0530

    removed apisecrete

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
     accountSid: 'AC2cc1717ec3e50507ec8267b315caf5f6',
     authToken: '',
     apiKey: 'SKcce7f18519f783cc741a5b0c3eceeb21',
-    apiSecret: 'YenZIXDOcmDwZNvX9vZExL0po3uJY2uL',
+    apiSecret: '',
     chatServiceSid: 'IS58750fba86ae4b898a4ccdc2fa57aea5'
   },
   port: process.env.PORT || 3001,
