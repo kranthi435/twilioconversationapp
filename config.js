42396c9fe18752ca3adbb34babc570f27676dd5c
Kranthi <kpeddi@aligntech.com>
2021-04-15 18:52:56 +0530

    removed authtoken

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ try {
 module.exports = {
   twilio: {
     accountSid: 'AC2cc1717ec3e50507ec8267b315caf5f6',
-    authToken: '0ea0c185d74ae42d15b05828c2676be4',
+    authToken: '',
     apiKey: 'SKcce7f18519f783cc741a5b0c3eceeb21',
     apiSecret: 'YenZIXDOcmDwZNvX9vZExL0po3uJY2uL',
     chatServiceSid: 'IS58750fba86ae4b898a4ccdc2fa57aea5'
